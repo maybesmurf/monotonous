27cb767f6709f2b691260c2447a19722608e0941
Nick Quebbeman <n.quebb@gmail.com>
2021-04-19 00:40:18 -0700

    make creating id links a little dry-er

--- apps/web/lib/graphcache.ts
+++ apps/web/lib/graphcache.ts
@@ -1,14 +1,19 @@
 import { IntrospectionData } from "@urql/exchange-graphcache/dist/types/ast";
-import { cacheExchange } from "@urql/exchange-graphcache";
+import { cacheExchange, Data } from "@urql/exchange-graphcache";
 import introspectedSchema from "schema_introspection.json";
 
 const schema = (introspectedSchema as unknown) as IntrospectionData;
+
+function resolveId(__typename: string) {
+  return (_: Data, args: { id: string }) => ({ __typename, id: args.id });
+}
 
 export const graphcache = cacheExchange({
   schema,
   resolvers: {
     Query: {
-      team: (_, args: { id: string }) => ({ __typename: "Team", ...args }),
+      team: resolveId("Team"),
+      project: resolveId("Project"),
     },
   },
 });
